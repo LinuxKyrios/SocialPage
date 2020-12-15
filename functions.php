ab8c4eef5b05f304e9df9cb67fb174b2d4dca473
Linux Kyrios <malinowskim92@protonmail.com>
2020-12-16 00:18:48 +0100

    typo improved

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@
     function destroySession()
     {
         $_SESSION=array();
-        if (session_id() != "" || isset($_COOKIE[session_name()])
+        if (session_id() != "" || isset($_COOKIE[session_name()]))
             setcookie(session_name(), '', time()-2592000, '/');
         session_destroy();
     }
